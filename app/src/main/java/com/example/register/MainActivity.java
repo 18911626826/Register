6789c0d9fbd32104568231f064cccbd49544a4a0
haitao.zhang <18911626826@163.com>
2019-05-30 18:56:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/register/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.register;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
